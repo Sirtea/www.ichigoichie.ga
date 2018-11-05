08a9c1d97627911bd9c3382881edbf72d02f461b
Gerard Monells <gerard.monells@gmail.com>
2018-11-05 11:40:44 +0100

    Enforce HTTPS

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.ichigoichie.tk'
+SITEURL = 'https://www.ichigoichie.tk'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
